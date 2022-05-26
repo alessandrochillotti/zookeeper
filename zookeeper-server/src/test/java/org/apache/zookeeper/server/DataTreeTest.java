688a75d6e651b224ebad84a848f628b4893019fe
alessandro <alessandro.chillotti@outlook.it>
2022-05-26 12:18:14 +0200

    added module aggregator

---
+++ zookeeper-server/src/test/java/org/apache/zookeeper/server/DataTreeTest.java
@@ -0,0 +1,14 @@
+package org.apache.zookeeper.server;
+
+import org.junit.Test;
+
+public class DataTreeTest {
+
+        public DataTreeTest() {
+        }
+
+        @Test
+        public void test() {
+        	DataTree dT = new DataTree();
+        }
+}
